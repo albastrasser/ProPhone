64c4954062644e179b65f16f74b9947266d4e29a
albastrasser <76761704+albastrasser@users.noreply.github.com>
2022-03-27 13:04:58 -0400

    API call returning images

--- src/Components/SearchField.jsx
+++ src/Components/SearchField.jsx
@@ -1,14 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const SearchField = () => {
   const [searchValue, setSearchValue] = useState('');
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    if (searchValue.length) {
+      const apiKey = process.env.REACT_APP_API_KEY;
+      const getImages = async () => {
+        try {
+          const apiResponse = await axios.get(
+            `https://pixabay.com/api/?key=${apiKey}&q=${searchValue}&image_type=photo`
+          );
+          console.log(apiResponse.data, 'data');
+        } catch (error) {
+          console.log(error, 'There was a problem getting images from Pixabay');
+        }
+      };
+      getImages();
+    }
+  }, [searchValue]);
+
   return (
     <div>
       <input
         type="search"
         placeholder="Search here"
         value={searchValue}
-        onChange={setSearchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
       />
     </div>
   );
