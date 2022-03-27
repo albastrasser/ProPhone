0c4b1c63386a50945f3836595ce4d302ece67872
albastrasser <76761704+albastrasser@users.noreply.github.com>
2022-03-27 13:57:30 -0400

    Added image detail page draft

--- src/Components/ImageDetail.jsx
+++ src/Components/ImageDetail.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
 export const ImageDetail = (props) => {
-  const { image } = props;
-  return <div>ImageDetail</div>;
+  const { image, setImageDetail } = props;
+  return (
+    <div>
+      <img
+        src={image.largeImageURL}
+        alt={`Enlarged view with the tags ${image.tags}`}
+      />
+      <h2>{image.tags}</h2>
+      <h3>by: {image.user}</h3>
+      <button onClick={() => setImageDetail(null)}>Close</button>
+    </div>
+  );
 };
 
 export default ImageDetail;
